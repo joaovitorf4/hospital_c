52e28637965ac6f50c08a864f98e89c5e383f16c
joaovitorf4 <joaovitorfod5@gmail.com>
2025-02-11 14:49:38 -0300

    submenu functions

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct {
+    int hora;
+    int minuto;
+} Horario;
+
+typedef struct {
+    char rua[50];
+    int numero;
+    char bairro[50];
+} Endereco;
+
 typedef struct {
     int id;
     char nome[50];
@@ -24,22 +42,75 @@ typedef struct {
     int duracao;
 } Consulta;
 
-typedef struct {
-    int dia;
-    int mes;
-    int ano;
-} Data;
+void subMenu(char *menu) {
+    int n;
 
-typedef struct {
-    int hora;
-    int minuto;
-} Horario;
+    do {
+        printf("1 - Inserir %s\n", menu);
+        printf("2 - Alterar %s\n", menu);
+        printf("3 - Excluir %s\n", menu);
+        printf("4 - Pesquisar %s\n", menu);
+        printf("5 - Sair\n");
 
-typedef struct {
-    char rua[50];
-    int numero;
-    char bairro[50];
-} Endereco;
+        scanf("%d", &n);
+
+        switch (n){
+            case 1:
+                printf("\nInserindo %s...\n", menu);
+                break;
+            case 2:
+                printf("\nAlterando %s...\n", menu);
+                break;
+            case 3:
+                printf("\nExcluindo %s...\n", menu);
+                break;
+            case 4:
+                printf("\nPesquisando %s...\n", menu);
+                break;
+            case 5:
+                printf("\nSaindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+        }
+    } while( n != 5 );
+}
+
+void relMenu(){
+    int n;
+
+    do {
+        printf("1 - Listar Consultas por Paciente\n");
+        printf("2 - Listar Consultas por Medico\n");
+        printf("3 - Listar Pacientes por Especialidade\n");
+        printf("4 - Sair\n");
+
+        scanf("%d", &n);
+
+        switch (n){
+            case 1:
+                printf("\nListando Consultas por Paciente...\n");
+                break;
+            case 2:
+                printf("\nListando Consultas por Medico...\n");
+                break;
+            case 3:
+                printf("\nListando Pacientes por Especialidade...\n");
+                break;
+            case 4:
+                printf("\nSaindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+        }
+    } while( n != 4 );
+}
+
+// void listConsultaByPaciente(){}
+// void listConsultaByMedico(){}
+// void listPacienteByEspecialidade(){}
 
 int main() {
     int n;
@@ -55,22 +126,26 @@ int main() {
 
         switch (n){
             case 1:
-                printf("Voce escolheu 1!\n");
+                printf("\nMenu das Consultas:\n");
+                subMenu("Consulta");
                 break;
             case 2:
-                printf("Voce escolheu 2!\n");
+                printf("\nMenu dos Pacientes:\n");
+                subMenu("Paciente");
                 break;
             case 3:
-                printf("Voce escolheu 3!\n");
+                printf("\nMenu dos Medicos:\n");
+                subMenu("Medico");
                 break;
             case 4:
-                printf("Voce escolheu 4!\n");
+                printf("\nMenu dos Relatorios:\n");
+                relMenu();
                 break;
             case 5:
-                printf("Saindo...\n");
+                printf("\nSaindo...\n");
                 break;
             default:
-                printf("Opcao invalida!\n");
+                printf("\nOpcao invalida!\n");
                 break;
         }
     } while ( n != 5 );
